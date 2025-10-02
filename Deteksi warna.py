29c57d68ac947f9e6618eb78f741596bc68edd7d
grimeroyal <the.rchm@gmail.com>
2025-10-02 18:21:26 +0700

    Update Deteksi warna.py

--- Deteksi warna.py
+++ Deteksi warna.py
@@ -21,9 +21,10 @@ with col1:
         st.session_state.v_range = (70, 255)
 
     # Slider HSV (pakai session_state)
-    h_min, h_max = st.slider("Hue Range", 0, 179, st.session_state.h_range, key="h_range_slider")
-    s_min, s_max = st.slider("Saturation Range", 0, 255, st.session_state.s_range, key="s_range_slider")
-    v_min, v_max = st.slider("Value Range", 0, 255, st.session_state.v_range, key="v_range_slider")
+    h_min, h_max = st.slider("Hue Range", 0, 179, value=st.session_state.h_range, key="h_range_slider")
+    s_min, s_max = st.slider("Saturation Range", 0, 255, value=st.session_state.s_range, key="s_range_slider")
+    v_min, v_max = st.slider("Value Range", 0, 255, value=st.session_state.v_range, key="v_range_slider")
+
 
     # Radio button preset warna
     st.subheader("Preset Cepat")
@@ -117,3 +118,4 @@ if uploaded_file is not None:
         file_name=f"hasil_deteksi.png",
         mime="image/png"
     )
+
